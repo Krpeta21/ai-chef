fc3de6e522c569fd4e32511fa64d0acf7c845cd2
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-02-05 13:20:17 -0600

    Modificacion RecipeCard

--- src/components/Recipe/RecipeCard.svelte
+++ src/components/Recipe/RecipeCard.svelte
@@ -1,10 +1,17 @@
 <script>
   export let food;
-  export let recipe;  
+  export let recipe;
+  const recipeS = recipe.split(",");
 </script>
 
-
-<article class="flex flex-col gap-10 border-2 border-blue-500 rounded-lg p-5 bg-white mt-10 mb-10">
-  <p>The recipe for <strong>{food}</strong> is:</p>
-  <p>{recipe}</p>
+<article
+  class="flex flex-col gap-10 border-2 border-blue-500 rounded-lg p-5 bg-white mt-10 mb-10"
+>
+  <p>The recipe for <strong class="text-blue-500 text-xl">{food}</strong> is:</p>  
+  <ul>
+    <p class="text-2xl font-bold text-blue-500">Ingredients:</p>
+    {#each recipeS as ingredient}
+      <li class="px-5 py-1">{ingredient}</li>    
+    {/each}    
+  </ul>
 </article>
